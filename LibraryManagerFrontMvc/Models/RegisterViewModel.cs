feb80c811cb94a09f1299cae1b8647b1d287eda4
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-07 21:40:46 +0100

    ajout confirm password modif du style d'affichage des livre en carte

--- LibraryManagerFrontMvc/Models/RegisterViewModel.cs
+++ LibraryManagerFrontMvc/Models/RegisterViewModel.cs
@@ -4,7 +4,8 @@ public class RegisterViewModel
 {
   public required string Email { get; set; } = string.Empty;
   public required string Password { get; set; } =  string.Empty;
-
+  
+  public required string ConfirmPassword { get; set; } =  string.Empty;
   public string FirstName { get; set; } = string.Empty;
   public string LastName { get; set; } = string.Empty;
 }
