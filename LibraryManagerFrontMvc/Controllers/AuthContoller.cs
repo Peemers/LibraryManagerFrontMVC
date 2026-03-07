430513a171986e1b1bf3db4cd34d7c7f63e35094
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-07 19:46:21 +0100

    Register.cshtml

--- LibraryManagerFrontMvc/Controllers/AuthContoller.cs
+++ LibraryManagerFrontMvc/Controllers/AuthContoller.cs
@@ -9,6 +9,7 @@ namespace LibraryManagerFrontMvc.Controllers;
 public class AuthController : Controller
 {
   private readonly IAuthService _authService;
+  private readonly HttpClient _httpClient;
 
   public AuthController(IAuthService authService)
   {
@@ -47,5 +48,31 @@ public class AuthController : Controller
 
     ModelState.AddModelError(string.Empty, "Email ou mot de passe incorrecte");
     return View(model);
+  }
+
+  [HttpGet]
+  public IActionResult Register()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Register(RegisterViewModel form)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(form);
+    }
+    var result = await _authService.CreateUser(form);
+
+    if (result != null)
+    {
+      TempData["Success"] = "Inscription reussie, vous pouvez maintenant vous connecter";
+      return RedirectToAction("Login", "Auth");
+    }
+    ModelState.AddModelError(string.Empty, "Erreur lors de l'inscription");
+    TempData["Error"] = "Echec d'inscription";
+    
+    return View(form);
   }
 }
