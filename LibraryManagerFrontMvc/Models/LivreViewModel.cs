17a43927f17835d5212442d81b030eb28343a19d
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-08 00:31:03 +0100

    dataAnnotation LivreViewModel.cs LoginViewModel.cs

--- LibraryManagerFrontMvc/Models/LivreViewModel.cs
+++ LibraryManagerFrontMvc/Models/LivreViewModel.cs
@@ -1,14 +1,36 @@
-﻿using LibraryManagerFrontMvc.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using LibraryManagerFrontMvc.Enums;
 
 namespace LibraryManagerFrontMvc.Models;
 
 public class LivreViewModel
 {
   public Guid Id { get; set; }
+  [Required]
+  [MinLength(2, ErrorMessage = "Minimum 2 Caracteres")]
+  [MaxLength(64, ErrorMessage = "Maximum 64 Caracteres")]
+  [Display(Name = "Titre")]
   public string Nom { get; set; } = string.Empty;
+  
+  [Required]
+  [MinLength(2, ErrorMessage = "Minimum 2 Caracteres")]
+  [MaxLength(64, ErrorMessage = "Maximum 64 Caracteres")]
+  [Display(Name = "Auteur")]
   public string Auteur { get; set; } = string.Empty;
+  
+  [Required]
   public int NbPages { get; set; } = 1;
+  
+  [Required]
+  [MinLength(12, ErrorMessage = "Minimum 12 Caracteres")]
+  [MaxLength(240, ErrorMessage = "Maximum 240 Caracteres")]
+  [Display(Name = "4eme de couverture")]
   public string Resume { get; set; } = string.Empty;
+  
+  [Required]
   public LivreStatut StatutLivre { get; set; }
+  
+  [Required]
+  [DataType(DataType.Date)]
   public DateTime DateDeSortie { get; set; }
 }
--- LibraryManagerFrontMvc/Models/LoginViewModel.cs
+++ LibraryManagerFrontMvc/Models/LoginViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace LibraryManagerFrontMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagerFrontMvc.Models;
 
 public class LoginViewModel
 {
+  [Required(ErrorMessage = "Email is required")]
+  [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", 
+    ErrorMessage = "Le format de l'email n'est pas valide")]
+  [MaxLength(45, ErrorMessage = "Max Length is 45")]
+  [Display(Name = "Adresse Email")]
   public required string Email { get; set; } = string.Empty;
+  
+  [Required(ErrorMessage = "Password is required")]
+  [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", 
+    ErrorMessage = "Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un caractère spécial.")]
+  [MinLength(6, ErrorMessage = "Min Length is 6")]
+  [MaxLength(20, ErrorMessage = "Max Length is 20")]
+  [Display(Name = "Mot de passe")]
+  [DataType(DataType.Password)]
   public required string Password { get; set; } =  string.Empty;
 }
