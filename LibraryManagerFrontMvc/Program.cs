1d0191ff541db43c8b6112db30ac7742a29e8ea7
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-08 12:45:43 +0100

    nettoyage service Program.cs

--- LibraryManagerFrontMvc/Program.cs
+++ LibraryManagerFrontMvc/Program.cs
@@ -1,36 +1,41 @@
 using LibraryManagerFrontMvc.Handlers;
 using LibraryManagerFrontMvc.Interfaces.Services;
 using LibraryManagerFrontMvc.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
-var apiBaseUrl = builder.Configuration["ApiSettings:BaseUrl"];
+var apiBaseUrl = builder.Configuration["ApiSettings:BaseUrl"] 
+    ?? throw new InvalidOperationException("Api BaseUrl manquante");
 
 #region Services
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(option =>
+{
+  option.IdleTimeout = TimeSpan.FromMinutes(30);
+    option.Cookie.HttpOnly = true;
+    option.Cookie.IsEssential = true;
+});
 
+builder.Services.AddTransient<AuthTokenHandler>();
 builder.Services.AddHttpClient<IAuthService, AuthService>(client =>
 {
-  client.BaseAddress = new Uri(apiBaseUrl ?? throw new InvalidOperationException("Api BaseUrl manquante"));
+  client.BaseAddress = new Uri(apiBaseUrl);
 });
 
 builder.Services.AddHttpClient<ILivreService, LivreService>(client =>
 {
-  client.BaseAddress = new Uri(apiBaseUrl ?? throw new InvalidOperationException("Api BaseUrl manquante"));
-});
-
-builder.Services.AddHttpClient<ILivreService, LivreService>(client => {
-  client.BaseAddress = new Uri("https://localhost:7001/");
+  client.BaseAddress = new Uri(apiBaseUrl);
 }).AddHttpMessageHandler<AuthTokenHandler>();
 
-builder.Services.AddAuthentication("CookieAuth")
-  .AddCookie("CookieAuth", config =>
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options =>
   {
-    config.Cookie.Name = "UserToken";
-    config.LoginPath = "/Auth/Login";
+    options.Cookie.Name = "UserToken";
+    options.LoginPath = "/Auth/Login";
+    options.AccessDeniedPath = "/Auth/AccessDenied";
   });
-
 #endregion
 
 var app = builder.Build();
